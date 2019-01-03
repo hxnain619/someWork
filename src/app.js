486c13c8b5f6882db9861ede52a10a564b31e598
hxnain619 <hxan619@gmail.com>
2019-01-03 14:36:23 +0500

    project completely deployed

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 class App extends React.Component {
   render() {
-    return <div className='App'>Hello React!</div>;
+    return <div >Hello React!</div>;
 
   }
 };
