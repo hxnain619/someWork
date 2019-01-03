e17106ae9a2f5301160f75f24bfc5d2215b356ee
hxnain619 <hxan619@gmail.com>
2019-01-03 12:42:18 +0500

    webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,28 @@
-const path = require("path");
-const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
-
-if (process.env.NODE_ENV === "test") {
-  require("dotenv").config({ path: ".env.test" });
-} else if (process.env.NODE_ENV === "development") {
-  require("dotenv").config({ path: ".env.development" });
-}
-
-module.exports = env => {
-  const isProduction = env === "production";
-  const CSSExtract = new ExtractTextPlugin("styles.css");
-
-  return {
-    entry: ["babel-polyfill", "./src/app.js"],
+const webpack = require('webpack');
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          }
+        ]
+      },
+      resolve: {
+        extensions: ['*', '.js', '.jsx']
+      },
     output: {
       path: __dirname + '/dist',
       publicPath: '/',
-      filename: "bundle.js"
-    },
-    module: {
-      rules: [
-        {
-          loader: "babel-loader",
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/
-        },
-        {
-          test: /\.s?css$/,
-          use: CSSExtract.extract({
-            use: [
-              {
-                loader: "css-loader",
-                options: {
-                  sourceMap: true
-                }
-              },
-              {
-                loader: "sass-loader",
-                options: {
-                  sourceMap: true
-                }
-              }
-            ]
-          })
-        }
-      ]
+      filename: 'bundle.js'
     },
     plugins: [
-      CSSExtract,
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.DefinePlugin({
-        "process.env.FIREBASE_API_KEY": JSON.stringify(
-          process.env.FIREBASE_API_KEY
-        ),
-        "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(
-          process.env.FIREBASE_AUTH_DOMAIN
-        ),
-        "process.env.FIREBASE_DATABASE_URL": JSON.stringify(
-          process.env.FIREBASE_DATABASE_URL
-        ),
-        "process.env.FIREBASE_PROJECT_ID": JSON.stringify(
-          process.env.FIREBASE_PROJECT_ID
-        ),
-        "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(
-          process.env.FIREBASE_STORAGE_BUCKET
-        ),
-        "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
-          process.env.FIREBASE_MESSAGING_SENDER_ID
-        )
-      })
-    ],
-    devtool: isProduction ? "source-map" : "inline-source-map",
+        new webpack.HotModuleReplacementPlugin()
+      ],
     devServer: {
       contentBase: './dist',
-      hot: true,
-      historyApiFallback: true
+      hot: true
     }
   };
-};
